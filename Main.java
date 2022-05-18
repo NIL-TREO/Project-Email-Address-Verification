25db8d4b8c546fc95dc414c6fe0603fc85a3f62b
NIL-TREO <101584989+NIL-TREO@users.noreply.github.com>
2022-05-18 10:34:51 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,9 @@
-package W_7_5_Email_Check;
+package EmailAddressVerification;
 
 public class Main {
-	/* This programm verifies the proper structure of a given e-mail address
-	 * It inspects the composition of local part, domain and top-level domain part
-	 * and if the general position of @ and dot-signs are correct
-	 */
+
 	public static void main(String[] args) {
 		// enter an e-mail address
-		CheckMailAddress email = new CheckMailAddress("aa@hallo.");
-						
+		CheckMailAddress email = new CheckMailAddress("abc@mail.com");				
 	}
 }
